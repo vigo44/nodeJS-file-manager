0977e603f57a40bfdf43a34b49a2945d943a83af
vigo44 <vvigo@rambler.ru>
2024-10-13 18:01:46 +0400

    feat: add parseName for start into powershell

--- src/common/parseUsername.js
+++ src/common/parseUsername.js
@@ -1,9 +1,11 @@
 export const parseUsername = () => {
   const args = process.argv.slice(2);
   const username =
+    process.env.npm_config_username ??
     args
       .find((item) => item.startsWith("--username="))
       ?.split("--username=")
-      ?.join("") ?? "anonymous";
+      ?.join("") ??
+    "anonymous";
   return username;
 };
